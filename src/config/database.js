616ed1353ed204d25dc18603fac5d73a0367bb13
sebelumcahahaya <anandadimmas1204@gmail.com>
2024-01-30 01:15:09 +0700

    perubahan env

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2");
 // require("dotenv").config();
+// console.log(process.env);
 // Create the connection pool. The pool-specific settings are the defaults
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
